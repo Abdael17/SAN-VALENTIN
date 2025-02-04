80ace93794adeafad4b847c6ad7a3b670457de99
Abdael17 <abdael172006@gmail.com>
2025-02-04 02:40:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("yesButton").addEventListener("click", function() {
-    document.getElementById("response").innerHTML = "<h2>¡YEEEEEE! 🎉💖</h2> <img src='https://media.tenor.com/OTKEn-MtV2EAAAAC/gato-cat.gif' alt='Gato feliz'>";
+    document.getElementById("response").innerHTML = "<h2>¡VAMOSSSSSSSSSSSSSSSSSSSS! 🎉💖</h2> <img src='https://media.tenor.com/OTKEn-MtV2EAAAAC/gato-cat.gif' alt='Gato feliz'>";
 });
 
 document.getElementById("noButton").addEventListener("click", function() {
-    document.getElementById("response").innerHTML = "<h2>😾 ¡Grrrr, qué cruel! 💔</h2> <img src='https://media.tenor.com/s7WJkzHC3ssAAAAC/angry-cat.gif' alt='Gato enojado'>";
+    document.getElementById("response").innerHTML = "<h2>😾 ¡QUE TONTA COMO QUE NO! 💔</h2> <img src='https://media.tenor.com/s7WJkzHC3ssAAAAC/angry-cat.gif' alt='Gato enojado'>";
 });
