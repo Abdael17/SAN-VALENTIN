a5c3703c2ddd57c475167886a4366663386b4683
Abdael17 <abdael172006@gmail.com>
2025-02-04 02:27:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    document.getElementById("response").innerHTML = "<h2>¡YEEEEEE! 🎉💖</h2> <img src='https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif' alt='Gato feliz'>";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    document.getElementById("response").innerHTML = "<h2>😾 ¡Grrrr, qué cruel! 💔</h2> <img src='https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif' alt='Gato enojado'>";
+});
