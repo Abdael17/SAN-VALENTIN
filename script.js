b8957f0cf3b071f69ab8e9e17880641c9462645a
Abdael17 <abdael172006@gmail.com>
2025-02-04 02:53:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("yesButton").addEventListener("click", function() {
-    document.getElementById("response").innerHTML = "<h2>¡VAMOSSSSSSSSSSSSSSSSSSSS! 🎉💖</h2> <img src='https://tenor.com/view/kiss-i-love-you-i-love-you-so-much-gif-13300892748761143712' alt='Gato feliz'>";
+    document.getElementById("response").innerHTML = "<h2>¡VAMOSSSSSSSSSSSSSSSSSSSS! 🎉💖</h2> <img src='https://media1.tenor.com/m/O4s7f1BDhWAAAAAd/monkies-hugging-happy.gif' alt='Monkey feliz'>";
 });
 
 document.getElementById("noButton").addEventListener("click", function() {
-    document.getElementById("response").innerHTML = "<h2>😾 ¡QUE TONTA COMO QUE NO! 💔</h2> <img src='https://tenor.com/view/gato-enojado-gif-14172878782844206746' alt='Gato enojado'>";
+    document.getElementById("response").innerHTML = "<h2>😾 ¡QUE TONTA COMO QUE NO! 💔</h2> <img src='https://media1.tenor.com/m/WpmYcNkPWBYAAAAC/fatuna-monkey.gif' alt='Monkey triste'>";
 });
