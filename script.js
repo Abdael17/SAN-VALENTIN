d756f6e97a88bb6e3d5495efe8883406e73de5d6
Abdael17 <abdael172006@gmail.com>
2025-02-04 02:26:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("noButton").addEventListener("mouseover", function() {
-    this.style.position = "absolute";
-    this.style.top = Math.random() * window.innerHeight + "px";
-    this.style.left = Math.random() * window.innerWidth + "px";
-});
-
-document.getElementById("yesButton").addEventListener("click", function() {
-    alert("¡Sabía que dirías que sí! ❤️");
-});
